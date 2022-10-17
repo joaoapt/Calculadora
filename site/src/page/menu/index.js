991284b2badb798b8d65b85d03900b422c89f437
João APT <104604232+joaoapt@users.noreply.github.com>
2022-10-17 18:17:46 -0300

    lol

--- site/src/page/menu/index.js
+++ site/src/page/menu/index.js
@@ -8,38 +8,39 @@ export default function Index() {
                 <h1>Menu</h1>
             </div>
             <div className='base'>
+                <br/>
                     <div>
                         <Link  className='botão' to='/acai'>1. Venda de Açai</Link>
+                        <br/> <br/> <br/> 
                     </div>
-                    <br/> <br/> 
                     <div>
                         <Link  className='botão' to='/libra'>2. Signo</Link>
-                    </div>
-                    <br/> <br/> 
+                        <br/> <br/> <br/> 
+                    </div> 
                     <div>
                         <Link  className='botão' to='/peso'>3. Venda de SORVETE</Link>
-                    </div>
-                    <br/> <br/> 
+                        <br/> <br/> <br/>
+                    </div>  
                     <div>
                         <Link  className='botão' to='/Salario'>4. Salario Liquido</Link>
+                        <br/> <br/> <br/> 
                     </div>
-                    <br/> <br/> 
                     <div>
                         <Link  className='botão' to='/abastecimento'>5.  Ipiranga mais procimo</Link>
+                        <br/> <br/> <br/> 
                     </div>
-                    <br/> <br/>
                     <div>
                         <Link  className='botão' to='/temperatura'>6. Temperatura</Link>
+                        <br/> <br/> <br/> 
                     </div>
-                    <br/> <br/>
                     <div>
                         <Link  className='botão' to='/orcamento'>7. Orçamento Familiar</Link>
+                        <br/> <br/> <br/> 
                     </div>
-                    <br/> <br/>
                     <div>
                         <Link  className='botão' to='/cinema'>8. Cinema</Link>
+                        <br/> <br/> <br/> 
                     </div>
-                    <br/> <br/>
             </div>
         </div>
     )
